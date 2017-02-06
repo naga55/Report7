d70696f702734772515f0852d0b7fed282060471
naga55 <e145741@ie.u-ryukyu.ac.jp>
2017-02-06 20:21:14 +0900

    second commit

--- src/main/e16/main.java
+++ src/main/e16/main.java
@@ -27,3 +27,63 @@ public class MYrobot extends AdvancedRobot
         }
     }
 
+    /**
+     * onScannedRobot: What to do when you see another robot
+     */
+    public void onScannedRobot(ScannedRobotEvent e) {
+        // Replace the next line with any behavior you would like
+        fire(3);
+        setTurnRightRadians(e.getBearingRadians() + Math.PI/2 - (e.getDistance()>200?Math.PI/8:-Math.PI/8));
+    }
+
+    /**
+     * onHitByBullet: What to do when you're hit by a bullet
+     */
+
+
+    /**
+     * onHitWall: What to do when you hit a wall
+     */
+    public void onHitWall(HitWallEvent e) {
+        // Replace the next line with any behavior you would like
+        double middleX = getBattleFieldWidth()/2;
+        double middleY = getBattleFieldWidth()/2;
+
+        double px = getX();
+        double py = getY();
+
+        double deltaX = middleX-px;
+        double deltaY = middleY-py;
+
+        double angle = 90-Math.floor(Math.toDegrees(Math.atan(deltaY/deltaX)));
+
+        px = getX();
+        py = getY();
+        deltaX = middleX-px+1;
+        deltaY = middleY-py;
+
+        if(Math.floor(Math.abs(deltaX))<=10&&Math.floor(Math.abs(deltaY))<=10){
+            angle = 0;
+            deltaX = 0;
+        }
+
+        turnRight(Math.abs(angle)-getHeading());
+        ahead(deltaX);
+    }
+
+    public void onBulletMissed(BulletMissedEvent event){
+        turnLeft(90);
+        ahead(100);
+    }
+    public void onHitByBullet(BulletHitEvent event){
+        turnLeft(90);
+        ahead(200);
+    }
+
+    public void onHitRobot(HitRobotEvent event){
+        if (event.getBearing() > -90 && event.getBearing() <= 90)
+            back(100);
+        else
+            ahead(100);
+    }
+}
